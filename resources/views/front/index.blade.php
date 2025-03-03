d8610f3d26458038806cf71027143d57984848e7
PutuRifkiDy <puturifki56@gmail.com>
2025-03-03 23:05:23 +0800

    update layouts

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -62,7 +62,7 @@
     </div>
 
     <!-- Bagian Gambar -->
-    <div class="w-max-full md:w-[900px] md:mt-40 mt-0 md:absolute right-0 md:right-0 overflow-hidden px-6 md:px-0 flex justify-center items-center"
+    <div class="w-max-full md:w-[900px] md:mt-40 mt-0 md:absolute md:right-0 overflow-hidden px-0 flex justify-center items-center"
         data-aos="fade-right">
         <img src="{{ asset('assets/photos/home-banner.png') }}" class="w-full h-auto md:w-auto" alt="Home Banner">
     </div>
