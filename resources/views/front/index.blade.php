32da07a058e7077af3c362e8964b52bb2dcea9f4
PutuRifkiDy <puturifki56@gmail.com>
2025-03-03 23:13:19 +0800

    test

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @section('title', 'Home')
 @include('components.navbar-front')
-<section id="content" class="flex md:flex-row flex-col-reverse justify-between items-center md:mt-24 mt-36 relative">
+<section id="content" class="flex md:flex-row flex-col-reverse justify-between items-center md:mt-24 mt-36 relative overflow-hidden">
     @if (session('success'))
         <div id="toast-simple"
             class="absolute top-5 right-0 md:left-24 left-0 w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse text-gray-500 bg-white divide-x rtl:divide-x-reverse divide-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:divide-gray-700 dark:bg-gray-800 z-50"
@@ -62,7 +62,7 @@
     </div>
 
     <!-- Bagian Gambar -->
-    <div class="w-max-full md:w-[900px] md:mt-40 mt-0 md:absolute md:right-0 overflow-x-hidden px-0 flex justify-center items-center"
+    <div class="md:w-[900px] md:mt-40 mt-0 md:absolute md:right-0 overflow-x-hidden px-0 flex justify-center items-center"
         data-aos="fade-right">
         <img src="{{ asset('assets/photos/home-banner.png') }}" class="w-full h-auto md:w-auto" alt="Home Banner">
     </div>
